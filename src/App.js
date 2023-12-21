d25a5e1272a63fdc41f64ce455c80582a10d5948
huythanh1324 <huythanh1324@gmail.com>
2023-12-21 20:04:26 +0700

    add signin signup and setting
    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
-
+import Signin from './pages/signin/Signin';
+import Signup from './pages/signup/Signup';
+import Setting from './pages/setting/Setting';
 function App() {
+  // const user = 1;
+  // const name = "Thanh";
+  // const phoneNum='0903710062'
+  // const email = 'huythanh1324@gmail.com'
   return (
     <div className="App">
-      
+      <Signin />
+      <Signup />
+      <Setting />
+      {/* <Setting user={user} name={name} phoneNum={phoneNum} email={email}/> */}
     </div>
   );
 }
